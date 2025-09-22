b0148b37231ee161f316a28ebfe03130b0a39e3d
Nakashi123 <nakajimakun1215@ymail.ne.jp>
2025-09-22 18:59:05 +0900

    Update PARMA_app (1).py

--- PARMA_app (1).py
+++ PARMA_app (1).py
@@ -19,6 +19,8 @@ from reportlab.lib.units import mm
 from reportlab.lib.utils import ImageReader
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+pip install reportlab
+
 
 
 # =========================
