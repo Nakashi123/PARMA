9527e7e389954ab618e2a604f6fdcc6fb1d63ba8
Nakashi123 <nakajimakun1215@ymail.ne.jp>
2025-12-29 10:12:21 +0900

    Fix image display parameters in PARMA_app

--- PARMA_app (1).py
+++ PARMA_app (1).py
@@ -274,8 +274,9 @@ if weak_keys:
     # 右：指定の画像を表示
     with col_right2:
         st.image(
-            "https://eiyoushi-hutaba.com/wp-content/uploads/2025/01/%E5%85%83%E6%B0%97%E3%81%AA%E3%82%B7%E3%83%8B%E3%82%A2%E3%81%AE%E4%BA%8C%E4%BA%BA%E3%80%80%E9%81%8B%E5%8B%95%E7%89%88.png",
-            use_container_width=True
-        )
+    "https://eiyoushi-hutaba.com/wp-content/uploads/2025/01/%E5%85%83%E6%B0%97%E3%81%AA%E3%82%B7%E3%83%8B%E3%82%A2%E3%81%AE%E4%BA%8C%E4%BA%BA%E3%80%80%E9%81%8B%E5%8B%95%E7%89%88.png",
+    use_column_width=True
+)
+
 
 st.markdown('</div>', unsafe_allow_html=True)
